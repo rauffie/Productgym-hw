3e05ff59632b8991aaf14d6ff3e25c1cd76c6a8c
rauffie <140356115+rauffie@users.noreply.github.com>
2023-07-30 09:31:56 -0400

    Add files via upload
    Product gym hw number 1, variables via javascript

---
+++ variable.js
@@ -0,0 +1,16 @@
+let firstName = "Raufin"
+console.log(firstName)
+let age = 31
+console.log(age)
+let isCurrentStudent = true
+console.log(isCurrentStudent)
+let favoriteColors = ["red", "blue", "black"]
+console.log(favoriteColors)
+let person = {
+    firstName: "Raufin",
+    lastName: "Chowdhury",
+    age: 31,
+    isCurrentStudent: true
+
+}
+console.log(person)
